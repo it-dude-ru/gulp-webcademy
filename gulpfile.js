788b1780511ff78d563e61e2b9437d61bca68234
it-dude-ru <it-dude@ya.ru>
2024-01-11 08:53:45 +0300

    sass compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
+const sass = require('gulp-sass')(require('sass'));
 
 const fileIncludeSettings = {
 	prefix: '@@',
@@ -10,4 +11,10 @@ gulp.task('includeFiles', function() {
 	return gulp.src('./src/*.html')
 		.pipe(fileInclude(fileIncludeSettings))
 		.pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('sass', function() {
+	return gulp.src('./src/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./dist/css/'));
 });
