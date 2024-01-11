4119324f847a1a33ff0921fc7256afb20648b1e6
it-dude-ru <it-dude@ya.ru>
2024-01-11 17:36:06 +0300

    task startServer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'));
+const server = require('gulp-server-livereload');
 
 const fileIncludeSettings = {
 	prefix: '@@',
@@ -21,4 +22,13 @@ gulp.task('sass', function() {
 
 gulp.task('copyImages', function() {
 	return gulp.src('./src/img/**/*').pipe(gulp.dest('./dist/img/'));
+});
+
+const serverOptions = {
+	livereload: true,
+	open: true
+};
+
+gulp.task('startServer', function() {
+	return gulp.src('./dist/').pipe(server(serverOptions));
 });
